811b60457355f2de7ac54f1d5b3debb6741a1388
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2024-01-24 21:11:46 +0300

    Add close() to class FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,9 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """
+        Fuction that deserializes a JSON file to objects
+        """
+        self.reload()
